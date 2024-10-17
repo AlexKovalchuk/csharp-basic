b3b3d9cb0ec91a84dd36100f1f10bb7e0e743131
Kovalchuk Oleksandr <oleksandr.kovalchuk@avenga.com>
2024-10-17 18:14:40 +0300

    Implement home work 12.0 resize and foreach availability

--- HomeWorks/HomeWorks/Program.cs
+++ HomeWorks/HomeWorks/Program.cs
@@ -1,28 +1,43 @@
 ﻿using HomeWorks;
 
-Console.WriteLine("Hello Home Work 11 -> My List");
+void CalledWhenResized(int newSize)
+{
+    Console.WriteLine($"List was resized to {newSize}");
+}
+
 var myList = new MyList<int>();
 myList.Add(1);
 myList.Add(2);
-foreach (var ml in myList)
+myList.OnResize += CalledWhenResized;
+myList.OnResizeFunc += int (int newSize) =>
 {
-    Console.WriteLine($"element = {ml}");
-}
-
+    Console.WriteLine($"Resized this using FUNC {newSize}");
+    return newSize;
+};
+myList.OnResizeAction += void (int newSize) =>
+{
+    Console.WriteLine($"Resized this using ACTION {newSize}");
+};
+myList.OnResizePredicate = bool (int newSize) =>
+{
+    Console.WriteLine($"Resized this using Predicate {newSize}");
+    return true;
+};
 myList[0] = 17;
 myList[1] = 347;
-Console.WriteLine($"[0] = {myList[0]} && [1] = {myList[1]}");
-Console.WriteLine($"Try to find 17 {myList.Find(17)}");
-Console.WriteLine($"try to remove 17 {myList.Remove(17)}");
-Console.WriteLine($"try to remove 17 {myList.Remove(17)}");
-Console.WriteLine($"Try to find 17 {myList.Find(17)}");
 myList.Remove(347);
 myList.Add(112);
 myList.Add(527);
 myList.Add(527);
 myList.Add(372);
-Console.WriteLine($"Try to find 372 {myList.Find(372)}");
-
+myList.Add(372);
+myList.Add(372);
+myList.Add(372);
+myList.Add(372);
+myList.Add(372);
+myList.Add(372);
+myList.Add(372);
+myList.Add(372);
 myList = myList + 5;
 myList = myList + 10;
 myList = myList - 527;
